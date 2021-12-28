3662569a06b1615fc2044f08db1a550eca084db1
Luca Pagano <lu.pagano@gmail.com>
2021-12-28 17:17:35 +0100

    example of trajectory added

--- swipe_modules/scanning_strategy.py
+++ swipe_modules/scanning_strategy.py
@@ -21,10 +21,11 @@ from litebird_sim.quaternions import (
 )
 from litebird_sim.imo import Imo
 from uuid import UUID
+import pkg_resources
 
+data_directory = pkg_resources.resource_filename('swipe_modules','data')
 
 EQUATOR_ECLIPTIC_ANGLE_RAD = 0.408407045  # 23.4 deg in radians
-
 
 @njit
 def SWIPE_spin_to_ecliptic(
